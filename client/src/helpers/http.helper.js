e87c5b77db185efd5d319511eccd1fe7c6b3a4e1
joysqwz <166097046+joysqwz@users.noreply.github.com>
2025-05-31 17:14:26 +0300

    .

--- client/src/helpers/http.helper.js
+++ client/src/helpers/http.helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'https://vueprj-sfmeied.ru//api'
+export const API_URL = 'https://vueprj-sfmeied.ru/api'
 
 const $api = axios.create({
   baseURL: API_URL,
